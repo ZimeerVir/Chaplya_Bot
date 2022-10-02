dfa80fd1111bac37ce40167d9c80b32023493530
ZimeerVir <sanya.viryasov.00.00@mail.ru>
2022-10-03 01:06:29 +0300

    Исправление кода

--- bot.py
+++ bot.py
@@ -16,23 +16,23 @@ dp = Dispatcher(bot)
 from config import open_weather_token, cities, week, month, code_to_smile
 from handlers import btns_menu, btns_weather, btns_chep
 
-# Строчка для проверки
-
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
+    # Начинаем общение бота с команды "/start"
     write_msg(message)
     mark = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton('Привет, Чапля! \U0001F44B')
     mark.add(button)
-    await bot.send_message(message.chat.id, f'\U0001F916 Ииии... Камера, мотор, погнали!', reply_markup=mark)
+    await bot.send_message(message.chat.id, '\U0001F916 Ииии... Камера, мотор, погнали!', reply_markup=mark)
 
-    msg_bot = f'\U0001F916 Ииии... Камера, мотор, погнали!'
+    msg_bot = '\U0001F916 Ииии... Камера, мотор, погнали!'
     write_msg_bot(message, msg_bot)
 
 
 @dp.message_handler(commands=['weather'])
 async def weather(msg: types.Message):
+    # Помощь о погоде (можно удалить)
     write_msg(msg)
     await bot.send_message(msg.chat.id, 'Введи, падла, свой гребанный город сюда:')
 
@@ -42,6 +42,7 @@ async def weather(msg: types.Message):
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
+    # Обработка сообщения и выбор ответа
     write_msg(msg)
     mess = msg.text.lower()
     if '111' in msg.text:
@@ -64,7 +65,7 @@ async def echo_message(msg: types.Message):
         write_msg_bot(msg, msg_bot)
     elif (msg.text == 'Погода \U00002600'):
         mark_weather = btns_weather()
-        await bot.send_message(msg.chat.id, f'Выбери город, чтобы узрить!', reply_markup=mark_weather)
+        await bot.send_message(msg.chat.id, 'Выбери город, чтобы узрить!', reply_markup=mark_weather)
         msg_bot = 'Выбери город, чтобы узрить!'
         write_msg_bot(msg, msg_bot)
     elif 'Чепухи' in msg.text:
@@ -73,7 +74,7 @@ async def echo_message(msg: types.Message):
 
         await bot.send_message(msg.chat.id, 'Есть ссылочки для тебя!', reply_markup=mark_chep)
         await bot.send_message(msg.chat.id, 'Хорошего дня)', reply_markup=mark_menu)
-        msg_bot = f'Есть ссылочки для тебя! Хорошего дня)'
+        msg_bot = 'Есть ссылочки для тебя! Хорошего дня)'
         write_msg_bot(msg, msg_bot)
     elif (msg. text == 'Мааася!) \U00002764'):
         await images_Masya(msg)
@@ -86,6 +87,7 @@ async def echo_message(msg: types.Message):
 
 
 async def data_weather(city, msg):
+    # Подключение к сервису погоды и обработка ответа
     r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric")
     data = r.json()
 
@@ -96,25 +98,27 @@ async def data_weather(city, msg):
     write_msg_bot(msg, msg_bot)
 
 async def date_now(data, msg):
+    # Работа со временем для раздела погоды
     tim = datetime.datetime.fromtimestamp(data['dt'])
-    time = tim.strftime('\n%H:%M:%S')
+    time_clock = tim.strftime('\n%H:%M:%S')
     date = tim.strftime("%d") + ' ' + month[tim.strftime("%B")] + ' ' + tim.strftime("%Y")
     day_week = week[tim.strftime("%A")]
 
     await bot.send_message(msg.from_user.id, data['name'])
-    await bot.send_message(msg.from_user.id, f'{time}\n{day_week}\n{date}')
-    msg_bot = f'{data["name"]} {time} {day_week} {date}'
+    await bot.send_message(msg.from_user.id, f'{time_clock}\n{day_week}\n{date}')
+    msg_bot = f'{data["name"]} {time_clock} {day_week} {date}'
     write_msg_bot(msg, msg_bot)
 
 
 async def weather_now(data, msg):
     # Хуйня ебанная, не буду так больше делать.(Про то, как было раньше) А вот солнце хз, как надо было
+    # Составление основного ответа о погоде
     mark_menu = btns_menu()
 
-    weather = data["weather"][0]["main"]
-    if weather in code_to_smile:
-        weather_smile = code_to_smile[weather]
-        msg_bot = f'{code_to_smile[weather]}\n'
+    weather_smile = data["weather"][0]["main"]
+    if weather_smile in code_to_smile:
+        weather_smile = code_to_smile[weather_smile]
+        msg_bot = f'{code_to_smile[weather_smile]}\n'
         write_msg_bot(msg, msg_bot)
     else:
         weather_smile = ("Посмотри в окно! А то чет не пойму, что там за погода!")
@@ -147,6 +151,7 @@ async def weather_now(data, msg):
 
 
 async def images_Masya(msg):
+    # Выбор рандомной картинки и ее отправка
     images = glob('./images/*')
     img = random.choice(images)
     await bot.send_photo(msg.chat.id, photo=open(img, 'rb'))
@@ -156,17 +161,20 @@ async def images_Masya(msg):
 
 
 def write_msg(msg):
-    file = open(f"./Messeges/Msg {msg.from_user.first_name}", "a")
+    # Запись в файл сообщений User
+    file = open(f"./Messeges/Msg {msg.from_user.first_name}", "a", encoding="UTF-8")
     file.write(f"{msg.date.date()}: {msg.from_user.first_name} ({msg.date.time()}) - {msg.text}\n")
     file.close()
 
 
 def write_msg_bot(msg, msg_bot):
-    file = open(f"./Messeges/Msg {msg.from_user.first_name}", "a")
+    # Запись в файл ответов бота 
+    file = open(f"./Messeges/Msg {msg.from_user.first_name}", "a", encoding="UTF-8")
     file.write(f"{msg.date.date()}: Chaplya_Bot ({msg.date.time()}) - {msg_bot}\n")
     file.close()
 
 
 if __name__ == '__main__':
+    # Я не понимаю (нужна хелп)
     executor.start_polling(dp)
 
