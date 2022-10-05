765fc3b6f8e2f7db70780a08037a4b6a2f92a5be
ZimeerVir <sanya.viryasov.00.00@mail.ru>
2022-10-06 02:47:13 +0300

    Добавлена типизация, исправлены мелочи кода

--- bot.py
+++ bot.py
@@ -1,66 +1,64 @@
 import asyncio
 import datetime
 import random
+import glob
 import re
 import requests
-
-from glob import glob
 
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-from aiogram.utils.exceptions import MessageNotModified
-
+from config import cities, code_to_smile, month, open_weather_token, week
+from config import help_answer
+from handlers import btns_chep, btns_menu, btns_weather
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-from config import cities, code_to_smile, month, open_weather_token, week
-from func_strings import anim
-from handlers import btns_chep, btns_menu, btns_weather
-
 
 @dp.message_handler(commands=["start"])
-async def process_start_command(message: types.Message):
+async def process_start_command(message: types.Message) -> None:
     """Начинаем общение бота с команды "/start" """
-    write_msg(message)
+    write_msg(message) # Запись сообщения пользователя в файл
+
     mark = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton("Привет, Чапля! \U0001F44B")
     mark.add(button)
+
     await bot.send_message(
         message.chat.id, "\U0001F916 Ииии... Камера, мотор, погнали!", reply_markup=mark
     )
-
-    msg_bot = "\U0001F916 Ииии... Камера, мотор, погнали!"
-    write_msg_bot(message, msg_bot)
+    write_msg_bot(message, "\U0001F916 Ииии... Камера, мотор, погнали!")
 
 
 @dp.message_handler(commands=["weather"])
-async def weather(msg: types.Message):
+async def weather(msg: types.Message) -> None:
     """Помощь о погоде (можно удалить)"""
-    write_msg(msg)
+    write_msg(msg) # Запись сообщения пользователя в файл
+
     await bot.send_message(msg.chat.id, "Введи, падла, свой гребанный город сюда:")
 
-    msg_bot = "Введи, падла, свой гребанный город сюда:"
-    write_msg_bot(msg, msg_bot)
+    write_msg_bot(msg, "Введи, падла, свой гребанный город сюда:")
 
 
 @dp.message_handler()
-async def echo_message(msg: types.Message):
+async def echo_message(msg: types.Message) -> None:
     """Обработка сообщения и выбор ответа"""
-    write_msg(msg)
+    write_msg(msg) # Запись сообщения пользователя в файл
+
     mess = msg.text.lower()
+
     if "111" in msg.text:
         await bot.delete_message(msg.chat.id, msg.message_id)
     elif "соси" in mess:
         await bot.delete_message(msg.chat.id, msg.message_id)
-        await bot.send_message(msg.chat.id, "\U0001F916 Сам !")
-        await asyncio.sleep(8)
+        await bot.send_message(msg.chat.id, "\U0001F916 Сам!")
+        await asyncio.sleep(5)
         await bot.delete_message(msg.chat.id, msg.message_id + 1)
         msg_bot = "Сам..."
         write_msg_bot(msg, msg_bot)
-    elif msg.text == "Привет, Чапля! \U0001F44B":
+    elif "привет" in mess:
         mark_menu = btns_menu()
         await bot.send_message(
             msg.chat.id,
@@ -69,89 +67,92 @@ async def echo_message(msg: types.Message):
             f"Посмотри на кнопки.",
             reply_markup=mark_menu,
         )
-        msg_bot = (
+        write_msg_bot(msg,
             f"Приветствую тебя, {msg.from_user.first_name}. \U0001F44B "
             f"Премного благодарен, что ты заглянул ко мне!) "
             f"Посмотри на кнопки."
         )
-        write_msg_bot(msg, msg_bot)
     elif msg.text == "Погода \U00002600":
         mark_weather = btns_weather()
         await bot.send_message(
             msg.chat.id, "Выбери город, чтобы узрить!", reply_markup=mark_weather
         )
-        msg_bot = "Выбери город, чтобы узрить!"
-        write_msg_bot(msg, msg_bot)
-    elif "Чепухи" in msg.text:
+        write_msg_bot(msg, "Выбери город, чтобы узрить!")
+    elif msg.text == "Чепухи \U0001F921":
         mark_menu = btns_menu()
         mark_chep = btns_chep()
-
         await bot.send_message(
             msg.chat.id, "Есть ссылочки для тебя!", reply_markup=mark_chep
         )
         await bot.send_message(msg.chat.id, "Хорошего дня)", reply_markup=mark_menu)
         msg_bot = "Есть ссылочки для тебя! Хорошего дня)"
-        write_msg_bot(msg, msg_bot)
+        write_msg_bot(msg, "Есть ссылочки для тебя! Хорошего дня)")
     elif msg.text == "Мааася!) \U00002764":
         await images_masya(msg)
-    elif "Поставь на" in msg.text:
+    elif msg.text == "Таймер \U0000231B":
+        await bot.send_message(msg.from_user.id,
+        'Чтобы поставить таймер, просто скажите мне "Поставь таймер на _ "')
+        write_msg_bot(msg, 'Чтобы поставить таймер, просто скажите мне "Поставь таймер на _ "')
+    elif msg.text == "Помощь \U0001F4AC":
+        mark_menu = btns_menu()
+        await bot.send_message(msg.from_user.id, help_answer, reply_markup=mark_menu)
+        write_msg_bot(msg, help_answer)
+    elif msg.text == "Запретка \U0001F64A":
+        mark_menu = btns_menu()
+        await bot.send_message(msg.from_user.id,
+        "Я культурный бот, поэтому попрошу не выражаться)", reply_markup=mark_menu)
+        write_msg_bot(msg, "Я культурный бот, поэтому попрошу не выражаться)")
+    elif "Поставь таймер на" in msg.text:
         await timer_10(msg)
     elif msg.text in cities:
         await data_weather(cities[msg.text], msg)
+    elif msg.text == "Назад \U0001F519":
+        mark_menu = btns_menu()
+        await bot.send_message(msg.from_user.id, "Жду указаний", reply_markup=mark_menu)
+        write_msg_bot(msg, "Жду указаний")
     else:
-        await bot.send_message(
-            msg.chat.id, "\U0001F916 Error: Ой! Давай не неси фигни!"
+        mark_menu = btns_menu()
+        await bot.send_message(msg.chat.id,
+        "\U0001F916 Error: Ой! Давай не неси фигни!", reply_markup=mark_menu
         )
-        msg_bot = "\U0001F916 Error: Ой! Давай не неси фигни!"
-        write_msg_bot(msg, msg_bot)
+        write_msg_bot(msg, "\U0001F916 Error: Ой! Давай не неси фигни!")
 
 
-async def data_weather(city, msg):
+async def data_weather(city: str, msg: types.Message) -> None:
     """Подключение к сервису погоды и обработка ответа"""
     request = requests.get(
-        f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric"
-    )
+        f"http://api.openweathermap.org/data/2.5/weather?q={city}"
+        f"&appid={open_weather_token}&units=metric")
     data = request.json()
-
-    await bot.send_message(msg.from_user.id, f"{request} \n")
     await date_now(data, msg)
     await weather_now(data, msg)
-    msg_bot = f"{request}"
-    write_msg_bot(msg, msg_bot)
+    write_msg_bot(msg, request)
 
 
-async def date_now(data, msg):
+async def date_now(data: dict, msg: types.Message) -> None:
     """Работа со временем для раздела погоды"""
     tim = datetime.datetime.fromtimestamp(data["dt"])
     time_clock = tim.strftime("\n%H:%M:%S")
     date = (
-        tim.strftime("%d") + " " + month[tim.strftime("%B")] + " " + tim.strftime("%Y")
-    )
+        tim.strftime("%d") + " " + month[tim.strftime("%B")] + " " + tim.strftime("%Y"))
     day_week = week[tim.strftime("%A")]
-
     await bot.send_message(msg.from_user.id, data["name"])
     await bot.send_message(msg.from_user.id, f"{time_clock}\n{day_week}\n{date}")
-    msg_bot = f'{data["name"]} {time_clock} {day_week} {date}'
-    write_msg_bot(msg, msg_bot)
+    write_msg_bot(msg, f'{data["name"]} {time_clock} {day_week} {date}')
 
 
-async def weather_now(data, msg):
+async def weather_now(data: dict, msg: types.Message) -> None:
     """Составление основного ответа о погоде"""
     mark_menu = btns_menu()
 
     weather_data = data["weather"][0]["main"]
     if weather_data in code_to_smile:
         weather_smile = code_to_smile[weather_data]
-        msg_bot = f"{code_to_smile[weather_data]}\n"
-        write_msg_bot(msg, msg_bot)
     else:
         weather_smile = "Посмотри в окно! А то чет не пойму, что там за погода!"
-        msg_bot = "Посмотри в окно! А то чет не пойму, что там за погода!"
-        write_msg_bot(msg, msg_bot)
-
+    write_msg_bot(msg, weather_smile)
     sun = datetime.datetime.fromtimestamp(
-        data["sys"]["sunset"]
-    ) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
 
     await bot.send_message(
         msg.from_user.id,
@@ -164,10 +165,9 @@ async def weather_now(data, msg):
         f'Восход: {datetime.datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M:%S")}\n'
         f'Закат: {datetime.datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M:%S")}\n'
         f"Продолжительность светового дня: {sun}",
-        reply_markup=mark_menu,
-    )
+        reply_markup=mark_menu)
 
-    msg_bot = (
+    write_msg_bot(msg,
         f"Погода: {weather_smile}"
         f'Температура: {data["main"]["temp"]}°C'
         f'Температура сегодня: {data["main"]["temp_min"]} - {data["main"]["temp_max"]}°C'
@@ -176,66 +176,51 @@ async def weather_now(data, msg):
         f"Время дня:"
         f'Восход: {datetime.datetime.fromtimestamp(data["sys"]["sunrise"]).strftime("%H:%M:%S")}'
         f'Закат: {datetime.datetime.fromtimestamp(data["sys"]["sunset"]).strftime("%H:%M:%S")}'
-        f"Продолжительность светового дня: {sun}"
-    )
-    write_msg_bot(msg, msg_bot)
+        f"Продолжительность светового дня: {sun}")
 
 
-async def images_masya(msg) -> None:
+async def images_masya(msg: types.Message) -> None:
     """Выбор рандомной картинки и ее отправка"""
-    images = glob("./images/*")
+    mark_menu = btns_menu()
+    images = glob.glob("./images/*")
     img = random.choice(images)
-    await bot.send_photo(msg.chat.id, photo=open(img, "rb"))
-
-    msg_bot = "*** Фото Марсена! ***"
-    write_msg_bot(msg, msg_bot)
+    await bot.send_photo(msg.chat.id, photo=open(img, "rb"), reply_markup=mark_menu)
+    write_msg_bot(msg, "*** Фото Марсена! ***")
 
 
-def write_msg(msg) -> None:
+def write_msg(msg: types.Message) -> None:
     """Запись в файл сообщений User"""
     file = open(f"./Messeges/Msg {msg.from_user.first_name}", "a", encoding="UTF-8")
-    file.write(
-        f"{msg.date.date()}: {msg.from_user.first_name} ({msg.date.time()}) - {msg.text}\n"
-    )
+    file.write(f"{msg.date.date()}: "
+    f"{msg.from_user.first_name} ({datetime.datetime.now().strftime('%H:%M:%S')}) - {msg.text}\n")
     file.close()
 
 
-def write_msg_bot(msg, msg_bot):
+def write_msg_bot(msg: types.Message, msg_bot: str) -> None:
     """Запись в файл ответов бота"""
     file = open(f"./Messeges/Msg {msg.from_user.first_name}", "a", encoding="UTF-8")
-    file.write(f"{msg.date.date()}: Chaplya_Bot ({msg.date.time()}) - {msg_bot}\n")
+    file.write(f"{msg.date.date()}: "
+    f"Chaplya_Bot ({datetime.datetime.now().strftime('%H:%M:%S')}) - {msg_bot}\n")
     file.close()
 
 
-async def timer_10(msg):
+async def timer_10(msg: types.Message) -> None:
     """(Разработка таймера)"""
-    x = re.findall(r"\d+", msg.text)
-    m = int(x[0])
-    print(m)
+    try:
+        time_user_list = re.findall(r"\d+", msg.text)
+        time_user_int = int(time_user_list[0])
 
-    n = 0
-    await bot.send_message(msg.from_user.id, m)
-    while n <= m:
-        n += 1
-        if n == m + 1:
-            await bot.send_message(msg.from_user.id, f"{m} SECONDS!")
-            break
+        await bot.send_message(msg.from_user.id, f"Пошел отсчет {time_user_int} сек.")
+        write_msg_bot(msg, f"Пошел отсчет {time_user_int} сек.")
 
-        num_proc = (100 * n) / m
-        num_dict = int(num_proc / 5)
-        print(f"n = {n}; proc = {int(num_proc)}; dict = {num_dict}")
-
-        # await bot.edit_message_text(anim[num_dict], msg.from_user.id, msg.message_id + 1)
-        try:
-            await bot.edit_message_text(
-                anim[num_dict], msg.from_user.id, msg.message_id + 1
-            )
-        except MessageNotModified:
-            await asyncio.sleep(1)
-            continue
-        await asyncio.sleep(1)
+        await asyncio.sleep(time_user_int)
+        await bot.send_message(msg.from_user.id, 'Время вышло!')
+        write_msg_bot(msg, "Время вышло!")
+    except IndexError:
+        await bot.send_message(msg.from_user.id, 'Я не понял, насколько ставить таймер')
 
 
 if __name__ == "__main__":
     # Я не понимаю (нужна хелп)
     executor.start_polling(dp)
+ 
--- handlers.py
+++ handlers.py
@@ -2,45 +2,46 @@ from aiogram import types
 
 
 def btns_menu():
+    """Объявление кнопок для основного меню"""
     mark_menu = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_weather = types.KeyboardButton("Погода \U00002600")
     btn_help = types.KeyboardButton("Помощь \U0001F4AC")
     btn_chep = types.KeyboardButton("Чепухи \U0001F921")
     btn_ban = types.KeyboardButton("Запретка \U0001F64A")
     btn_img = types.KeyboardButton("Мааася!) \U00002764")
+    btn_timer = types.KeyboardButton("Таймер \U0000231B")
     mark_menu.add(btn_weather)
     mark_menu.add(btn_chep)
     mark_menu.add(btn_img)
+    mark_menu.add(btn_timer)
     mark_menu.add(btn_help, btn_ban)
     return mark_menu
 
 
 def btns_weather():
+    """Объявление кнопок для выбора города в погоде"""
     mark_weather = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn_moscow = types.KeyboardButton("Москва")
     btn_zelenograd = types.KeyboardButton("Зеленоград")
     btn_saratov = types.KeyboardButton("Саратов")
     btn_balakovo = types.KeyboardButton("Балаково")
     btn_volsk = types.KeyboardButton("Вольск")
+    btn_cancel = types.KeyboardButton("Назад \U0001F519")
     mark_weather.add(btn_moscow)
     mark_weather.add(btn_zelenograd)
     mark_weather.add(btn_saratov)
     mark_weather.add(btn_balakovo)
     mark_weather.add(btn_volsk)
+    mark_weather.add(btn_cancel)
     return mark_weather
 
 
 def btns_chep():
+    """Компоновка ссылок-кнопок для раздела 'Чепухи'"""
     mark_chep = types.InlineKeyboardMarkup()
-    btn_chep_1 = types.InlineKeyboardButton(
-        "Первый чепух!", url="https://vk.com/elkinay"
-    )
-    btn_chep_2 = types.InlineKeyboardButton(
-        "Второй чепух!", url="https://vk.com/zimeervir"
-    )
-    btn_chep_3 = types.InlineKeyboardButton(
-        "Третий чепух!", url="https://vk.com/id299453853"
-    )
+    btn_chep_1 = types.InlineKeyboardButton("Первый чепух!", url="https://vk.com/elkinay")
+    btn_chep_2 = types.InlineKeyboardButton("Второй чепух!", url="https://vk.com/zimeervir")
+    btn_chep_3 = types.InlineKeyboardButton("Третий чепух!", url="https://vk.com/id299453853")
     mark_chep.add(btn_chep_1)
     mark_chep.add(btn_chep_2, btn_chep_3)
     return mark_chep
